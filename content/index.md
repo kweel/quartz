2c4da25a9fa91457b42d2d1639d703efc596ba4e
Kevin (Woohyeuk) Lee <ke_vinlee@outlook.com>
2023-11-08 10:10:59 -0600

    Quartz sync: Nov 8, 2023, 10:10 AM

--- content/index.md
+++ content/index.md
@@ -1,11 +1,11 @@
 ---
 title: Table of Contents
 ---
+[[Research]] - find my research statement, projects, and related writings
 
-(Website currently under reconstruction)
+[[Wander]] - (almost) daily updates of misc. writing
+
+[[Fun]] - book reviews, photography, music, and more
 
 ![[P4210022 1.jpg]]
 
-- [[Books]]
-- [[Photography]]
-- [[Digital Music Production]]
--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -5,6 +5,10 @@ import { resolveRelative, simplifySlug } from "../util/path"
 function Backlinks({ fileData, allFiles, displayClass }: QuartzComponentProps) {
   const slug = simplifySlug(fileData.slug!)
   const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+  // filter out for everything except index, code from jzhao's site
+  if (fileData.slug == "index") {
+    return <></>
+  }
   return (
     <div class={`backlinks ${displayClass ?? ""}`}>
       <h3>Backlinks</h3>
