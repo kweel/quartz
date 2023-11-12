c59e6481e941f76c520dba058cc97c2215e1745d
Kevin (Woohyeuk) Lee <ke_vinlee@outlook.com>
2023-11-11 19:14:05 -0600

    Quartz sync: Nov 11, 2023, 7:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Woohyeuk Lee",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
